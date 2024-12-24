9253c77f87b7d5d2343626d9ebdc391f4c83b85b
Sarvar Omonov <sarvaromon33@gmail.com>
2024-12-25 00:15:57 +0500

    I made some changes to the main main.py but it's not complete yet.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import aiogram
+import requests
 import config
 import asyncio
 from aiogram import Bot, Dispatcher, types, executor
@@ -9,8 +10,11 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 # Botni yaratamiz
 bot = Bot(token=config.BOT_TOKEN)
 dp = Dispatcher(bot)
+# Apining url manzili
+url = "https://google-translate1.p.rapidapi.com/language/translate/v2"
 
 
+# 
 #@dp.message_handler(commands=["start"])  # Start command
 #async def echo_handler(message: Message):
 #    first_name = message.from_user.first_name  # Get user's 
@@ -35,6 +39,27 @@ def get_translation_buttons():
     return keyboard
 
 
+@dp.message_handler()
+def get_text_from_user(message: Message):
+    user_text = message.text
+    print(user_text)
+
+
+payload = {
+	"q": "Salom, dunyo!",
+	"target": "en",
+	"source": "uz"
+}
+headers = {
+	"x-rapidapi-key": "707c9a7aeemsh5ffec9ad5b7ac26p1582a5jsn2dd2b6a6b87b",
+	"x-rapidapi-host": "google-translate1.p.rapidapi.com",
+	"Content-Type": "application/x-www-form-urlencoded",
+	"Accept-Encoding": "application/gzip"
+}
+
+response = requests.post(url, data=payload, headers=headers)
+
+print(response.json())
 
 
 # Botni ishga tushirish
